a380504a4ef45e0458190f42e8a5ff480002fdef
Nikita Ablamskiy <shealtov@gmail.com>
2024-07-02 02:36:34 +0300

    Formatting in home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4,15 +4,16 @@
     ./modules/bundle.nix
   ];
 
-  home = {
-    username = "fly0utwest";
-    homeDirectory = "/home/fly0utwest";
-    stateVersion = "24.05";
-  };
   xdg = {
     userDirs = {
       enable = true;
       createDirectories = true;
     };
+  };
+
+  home = {
+    username = "fly0utwest";
+    homeDirectory = "/home/fly0utwest";
+    stateVersion = "24.05";
   };
 } 

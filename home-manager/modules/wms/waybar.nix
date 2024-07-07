0769351954399bd6e63708c76628200ab913a798
Nikita Ablamskiy <shealtov@gmail.com>
2024-07-07 19:09:32 +0300

    Waybar border-radius and margin fixed
    Margin-right instead of margin for clock waybar module

--- home-manager/modules/wms/waybar.nix
+++ home-manager/modules/wms/waybar.nix
@@ -223,7 +223,8 @@ window#waybar.hidden {
 #clock {
     padding-left: 16px;
     padding-right: 16px;
-    border-radius: 10px 0px 0px 10px;
+    border-radius: 10px;
+    margin-right: 8px;
     transition: none;
     color: #ffffff;
     background: #383c4a;
